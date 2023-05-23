2fbc33d9fe8c57e6458a51bfecba154ae1fb18e2
Marcos <marcosmelo012@gmail.com>
2023-05-23 18:15:54 -0300

    feat: initial project

---
+++ front/src/App.tsx
@@ -0,0 +1,10 @@
+
+export function App() {
+
+  return (
+    <>
+      <h1>hello world!</h1>
+    </>
+  )
+}
+
---
+++ front/src/routes/index.tsx
@@ -0,0 +1,10 @@
+import { Route, Routes } from "react-router-dom"
+
+export const RoutesMain = () => {
+    return (
+        <Routes>
+            <Route path="/" />
+            <Route path="/register" />
+        </Routes>
+    )
+}
