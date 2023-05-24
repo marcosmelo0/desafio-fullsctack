e5ad88d9cfa2c57625a8b700605a012f5f16962f
Marcos <marcosmelo012@gmail.com>
2023-05-24 19:41:21 -0300

    feat: add page register structure

--- front/src/pages/login/index.tsx
+++ front/src/pages/login/index.tsx
@@ -8,54 +8,52 @@ export const Login = () => {
     navigate("/register");
   };
   return (
-    <>
-      <div
-        className={`text-slate-700 bg-gradient-to-br from-blue-950 via-blue-900 to-blue-950 h-screen`}
-      >
-        <div className="w-[90%] mt-[25vh] ml-[5%] flex h-auto pb-10 pt-5 flex-col bg-white rounded-[10px] absolute items-center justify-center">
-          <img className="w-16" src={logo} alt="logo contact flow" />
-          <h1>| Contact Flow - Login |</h1>
-          <div className="w-[90%]">
-            <form
-              action="submit"
-              className="border-gray-300 rounded-[10px] mt-5 p-2 pb-5 border-2 flex flex-col gap-2"
+    <main
+      className={`text-slate-700 bg-gradient-to-br from-blue-950 via-blue-900 to-blue-950 h-screen`}
+    >
+      <div className="w-[90%] mt-[25vh] ml-[5%] flex h-auto pb-10 pt-5 flex-col bg-white rounded-[10px] absolute items-center justify-center">
+        <img className="w-16" src={logo} alt="logo contact flow" />
+        <h1 className="text-[17px] font-semibold">| Contact Flow - Login |</h1>
+        <div className="w-[90%]">
+          <form
+            action="submit"
+            className="border-gray-300 rounded-[10px] mt-5 p-3 pb-5 border-2 flex flex-col gap-2"
+          >
+            <label htmlFor="email" className="text-slate-700 ">
+              E-mail:
+            </label>
+            <input
+              type="email"
+              id="email"
+              placeholder="Digite seu e-mail"
+              className="border-2 placeholder-gray-400 pl-2 h-10 w-full rounded-[5px] focus:outline-blue-600 transition-all duration-1000"
+            />
+            <label htmlFor="password" className="text-slate-700 ">
+              Senha:
+            </label>
+            <input
+              type="password"
+              id="password"
+              placeholder="Digite sua senha"
+              className="border-2 placeholder-gray-400 pl-2 h-10 w-full rounded-[5px] focus:outline-blue-600 transition-all duration-1000"
+            />
+            <button
+              type="submit"
+              className="border-2 rounded-full h-10 mt-2 bg-blue-700 hover:bg-blue-800 text-white"
             >
-              <label htmlFor="email" className="text-slate-700 ">
-                E-mail:
-              </label>
-              <input
-                type="email"
-                id="email"
-                placeholder="Digite seu e-mail"
-                className="border-2 placeholder-gray-400 pl-2 h-10 w-full rounded-[5px] focus:outline-blue-600 transition-all duration-1000"
-              />
-              <label htmlFor="password" className="text-slate-700 ">
-                Senha:
-              </label>
-              <input
-                type="password"
-                id="password"
-                placeholder="Digite sua senha"
-                className="border-2 placeholder-gray-400 pl-2 h-10 w-full rounded-[5px] focus:outline-blue-600 transition-all duration-1000"
-              />
-              <button
-                type="submit"
-                className="border-2 rounded-full h-10 mt-2 bg-blue-700 hover:bg-blue-800 text-white"
-              >
-                Entrar
-              </button>
-              <span className="text-center">Não tem conta?</span>
-              <button
-                type="button"
-                onClick={handleRegister}
-                className="border-2 rounded-full h-10 mt-2 bg-blue-700 hover:bg-blue-800 text-white"
-              >
-                Cadastrar
-              </button>
-            </form>
-          </div>
+              Entrar
+            </button>
+            <span className="text-center">Não tem conta?</span>
+            <button
+              type="button"
+              onClick={handleRegister}
+              className="border-2 rounded-full h-10 mt-2 bg-blue-700 hover:bg-blue-800 text-white"
+            >
+              Cadastrar
+            </button>
+          </form>
         </div>
       </div>
-    </>
+    </main>
   );
 };
--- front/src/pages/register/index.tsx
+++ front/src/pages/register/index.tsx
@@ -1,5 +1,38 @@
+import logo from "../../assets/img/logo.png";
+import { IMaskInput } from "react-imask";
+
 export const Register = () => {
-    return (
-        <h1>Page Register!</h1>
-    )
-}
+  return (
+    <>
+      <main className="bg-gradient-to-br from-blue-950 via-blue-900 to-blue-950 h-screen">
+        <div className="w-[90%] ml-[5%] mt-[25vh] rounded-[10px] bg-white h-auto absolute">
+          <div className="flex flex-col pb-5 pt-5 justify-center items-center">
+            <img className="w-16" src={logo} alt="logotipo" />
+            <h1 className="text-slate-700 font-semibold text-[18px]">| Contact Flow - Register |</h1>
+          </div>
+          <form className="w-[90%] ml-[5%] border-gray-300 rounded-[10px] mt-5 p-3 mb-5 border-2 flex flex-col gap-2">
+            <label htmlFor="name">Nome completo:</label>
+            <input
+              type="text"
+              id="name"
+              placeholder="Digitar seu nome"
+              className="border-2 placeholder-gray-400 pl-2 h-10 w-full rounded-[5px] focus:outline-blue-600 transition-all duration-1000"
+            />
+            <label htmlFor="email">E-mail:</label>
+            <input
+              type="email"
+              id="email"
+              placeholder="Digitar seu e-mail"
+              className="border-2 placeholder-gray-400 pl-2 h-10 w-full rounded-[5px] focus:outline-blue-600 transition-all duration-1000"
+            />
+            <label htmlFor="telephone">Telefone:</label>
+            <IMaskInput mask="(00) 0 0000-0000" placeholder="Digite seu número" className="border-2 placeholder-gray-400 pl-2 h-10 w-full rounded-[5px] focus:outline-blue-600 transition-all duration-1000"/>
+            <button className="border-2 rounded-full h-10 mt-2 bg-blue-700 hover:bg-blue-800 text-white">Cadastrar</button>
+            <span className="m-auto">Já tem conta?</span>
+            <button className="border-2 rounded-full h-10 mt-2 bg-blue-700 hover:bg-blue-800 text-white">Entrar</button>
+          </form>
+        </div>
+      </main>
+    </>
+  );
+};
